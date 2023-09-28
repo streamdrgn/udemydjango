338cf8d9d2714df57c71a6c209a265d8aaae150f
streamdrgn <sjhs6223@naver.com>
2023-09-29 01:21:05 +0900

    0929 rendering

--- udemydjango/views.py
+++ udemydjango/views.py
@@ -2,7 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "global/base.html")
+    my_title = "Hello there..."
+    return render(request, "base.html")
 
 def about_page(request):
-    return HttpResponse("<h1>This is about page!</h1>")
+    return render(request, "about.html", {"title": "About"})
+
+def example_page(request):
+    context = {"title": "Example"}
+    something_here = "hello_world.html"
+    return HttpResponse(something_here) render(request, "hello_world.html", {"title": "Contact us"})
